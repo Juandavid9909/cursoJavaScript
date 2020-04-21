36445eb0d9a7263a4663327ce1fd08b2938609f1
Juan David <juandavid918@gmail.com>
2020-04-20 20:15:43 -0500

    Introducción a NodeJS

---
+++ introNodeJS/server/index.js
@@ -0,0 +1,11 @@
+// Importar express
+const express = require("express");
+
+// Configurar express
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Hola mundo en NodeJS");
+});
+
+app.listen(3000);
